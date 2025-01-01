7e8890c6a424025ab0f50ef24f071aa1077a8b3e
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-01 11:56:01 +0600

    main section daone

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import MainPage from "@/components/MainPage/MainPage";
 import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <MainPage />
     </>
   );
 }
