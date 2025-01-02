cd53732da01470ddca941cdc34fbf6760d324f2f
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-02 10:12:24 +0600

    home page done

--- src/components/Review/OurReview.jsx
+++ src/components/Review/OurReview.jsx
@@ -1,28 +1,48 @@
-import reviewSvg from "@/images/svg/undraw_review_ofim.svg";
-import Image from "next/image";
+"use client";
+
+import loadingAnimation from "@/assert/svg/review.json";
+import lottie from "lottie-web";
+import { useEffect, useRef } from "react";
 import OurReviewSlider from "./OurReviewSlider";
 import "./style.css";
 export default function OurReview() {
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    const animationInstance = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: loadingAnimation,
+    });
+
+    // Clean up the animation on component unmount
+    return () => animationInstance.destroy();
+  }, []);
   return (
     <section className="container mx-auto px-2 py-10">
       <div className="">
         <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-5">
           <div className="">
             <div className="">
-              <div className="w-[98%] sm:w-[50%] h-auto mr-auto ml0 mx-auto ">
-                <Image className="w-full" src={reviewSvg} alt="loading"></Image>
+              <div className="w-[98%] sm:w-[50%] h-auto   mx-auto ">
+                <div
+                  ref={animationContainer}
+                  className="w-full h-auto mx-auto"
+                ></div>
               </div>
               <div className="px-4 pt-5 ">
                 <h1 className="text-3xl font-medium pb-6 sm:pb-0">
                   <strong className="text-4xl font-semibold text-slate-800">
-                    সম্মানীত
+                    Student
                   </strong>{" "}
-                  গ্রাহকদের অভিমত
+                  Feedback
                 </h1>
                 <p className="pt-5">
-                  আমাদের পণ্য ও সেবার মান নিয়ে সম্মানিত গ্রাহকদের মূল্যবান মতামত
-                  ও অভিজ্ঞতা। আমরা প্রতিটি মন্তব্যকে গুরুত্ব দিয়ে বিবেচনা করি,
-                  যাতে আপনাদের সন্তুষ্টি নিশ্চিত করতে পারি।
+                  Your journey begins here. See how our students describe their
+                  experiences and how we’ve helped them excel academically and
+                  personally.
                 </p>
               </div>
             </div>
