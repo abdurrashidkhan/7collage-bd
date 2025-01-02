248cb3ce42c4b1353a3620da44225a3d870c20fe
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-02 12:10:36 +0600

    studen admition form added

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
 import CollegeGoals from "@/components/7CollegeGoals/CollegeGoals";
 import CollegeFeaturesSection from "@/components/CollegeFeaturesSection.jsx/CollegeFeaturesSection";
-import Footer from "@/components/Footer/Footer";
 import GraduateGallery from "@/components/GraduateGallery/GraduateGallery";
 import MainPage from "@/components/MainPage/MainPage";
-import Navbar from "@/components/Navbar/Navbar";
 import OurReview from "@/components/Review/OurReview";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+
       <MainPage />
       <CollegeGoals />
       <CollegeFeaturesSection />
       <GraduateGallery />
       <OurReview />
-      <Footer />
     </>
   );
 }
