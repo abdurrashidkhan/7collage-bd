e4e4d3710ea406e914d3177cb8b977b896059f84
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-03 15:59:37 +0600

    update here

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import CollegeGoals from "@/components/7CollegeGoals/CollegeGoals";
+import CollegeFeaturesSection from "@/components/CollegeFeaturesSection.jsx/CollegeFeaturesSection";
+import GraduateGallery from "@/components/GraduateGallery/GraduateGallery";
+import MainPage from "@/components/MainPage/MainPage";
+import OurReview from "@/components/Review/OurReview";
+
+export default function Home() {
+  return (
+    <>
+      <MainPage />
+      <CollegeGoals />
+      <CollegeFeaturesSection />
+      <GraduateGallery />
+      <OurReview />
+    </>
+  );
+}
