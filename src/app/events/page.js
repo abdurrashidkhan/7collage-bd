66e5e18cc20742004533dd4ca87ad34422c52941
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-03 14:26:52 +0600

    add event and get all event

---
+++ src/app/events/page.js
@@ -0,0 +1,18 @@
+
+export default function page() {
+  return (
+    <section>
+      <div className="container mx-auto px-4 py-10">
+        <div className="">
+          <div className="">
+            <h1>Our Collages Events</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quam cum eius expedita commodi debitis libero nihil voluptas, perspiciatis minima iure harum aspernatur facilis voluptatum exercitationem porro corrupti dolores ipsa nisi autem, quod praesentium minus? Qui facere vitae ipsum quasi quisquam modi sequi, ea, quia, ducimus optio voluptas. Amet, tempora.</p>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+            {/* data find  */}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
